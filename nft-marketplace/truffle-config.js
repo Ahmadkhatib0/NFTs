4dd86a705f66a9237792dfd396a17c4a97a03120
ahmadkhatib <Ahmadkhatib0@gmail.com>
2021-12-16 22:36:00 +0300

    a sample contract, and deploy it to the blockchain by truffle compile and then truffle megrate

--- nft-marketplace/truffle-config.js
+++ nft-marketplace/truffle-config.js
@@ -1,18 +1,20 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*",
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
     },
-    contracts_directory: "./src/contracts",
-    contracts_build_directory: "./src/abis",
   },
+
+  contracts_directory: "./src/contracts/",
+  contracts_build_directory: "./src/abis/",
+
   compilers: {
     solc: {
       version: "^0.8.0",
       optimizer: {
-        enabled: true,
+        enabled: "true",
         runs: 200,
       },
     },
