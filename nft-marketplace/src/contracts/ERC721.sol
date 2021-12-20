c33b0fd6f21ae36253222e339ecc698d34ba182d
ahmadkhatib <Ahmadkhatib0@gmail.com>
2021-12-20 20:54:07 +0300

    add the ERC721Enumerable contract

--- nft-marketplace/src/contracts/ERC721.sol
+++ nft-marketplace/src/contracts/ERC721.sol
@@ -28,7 +28,7 @@ contract ERC721 {
         return owner;
     }
 
-    function _mint(address to, uint256 tokenId) internal {
+    function _mint(address to, uint256 tokenId) internal virtual {
         require(to != address(0), "ERC721: minting to the zero address");
         require(!_exists(tokenId), "ERC721: token already minted");
         _tokenOwner[tokenId] = to;
