f9cc418fe39f2bbe0947803f1eb137b594cd863b
ahmadkhatib <Ahmadkhatib0@gmail.com>
2021-12-26 17:22:05 +0300

    transferFrom function

--- nft-marketplace/src/contracts/ERC721.sol
+++ nft-marketplace/src/contracts/ERC721.sol
@@ -8,9 +8,17 @@ contract ERC721 {
         address indexed to,
         uint256 indexed tokenId
     );
+
+    // event Approval(
+    //     address indexed owner,
+    //     address indexed approve,
+    //     uint256 indexed tokenId
+    // );
+
     mapping(uint256 => address) private _tokenOwner;
     // mapping from owner to number of owned tokens
     mapping(address => uint256) private _ownedTokensCount;
+    mapping(uint256 => address) private _tokenApprovals;
 
     function _exists(uint256 tokenId) internal view returns (bool) {
         address owner = _tokenOwner[tokenId];
@@ -22,7 +30,7 @@ contract ERC721 {
         return _ownedTokensCount[_owner];
     }
 
-    function ownerOf(uint256 tokenId) external view returns (address) {
+    function ownerOf(uint256 tokenId) public view returns (address) {
         address owner = _tokenOwner[tokenId];
         require(owner != address(0), "owner query for now-existent token");
         return owner;
@@ -35,5 +43,42 @@ contract ERC721 {
         //  _ownedTokensCount[to] = _ownedTokensCount[to] + 1 ; //or
         _ownedTokensCount[to] += 1;
         emit Transfer(address(0), to, tokenId);
+    }
+
+    function _transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) internal {
+        require(
+            _to != address(0),
+            "Error ERC721, transfer to the zero address"
+        );
+        require(
+            ownerOf(_tokenId) == _from,
+            "Trying to transfer a token the address does not own "
+        );
+        _ownedTokensCount[_from] -= 1;
+        _ownedTokensCount[_to] += 1;
+        _tokenOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    // function approve(address _to, uint256 tokenId) {
+    //     address owner = ownerOf(tokenId);
+    //     require(_to != owner, "Error- approval current owner ");
+    //     require(
+    //         msg.sender == owner,
+    //         "current caller is not the owner of the token"
+    //     );
+    //     _tokenApprovals[tokenId] = _to;
+    // }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public {
+        _transferFrom(_from, _to, _tokenId);
     }
 }
--- nft-marketplace/src/contracts/ERC721Enumerable.sol
+++ nft-marketplace/src/contracts/ERC721Enumerable.sol
@@ -22,11 +22,33 @@ contract ERC721Enumerable is ERC721 {
 
     function _mint(address to, uint256 tokenId) internal override(ERC721) {
         super._mint(to, tokenId);
-        _addTokensToTotalSupply(tokenId);
+        _addTokensToAllTokensEnumeration(tokenId);
+        _addTokensToOwnerEnumeration(to, tokenId);
     }
 
-    function _addTokensToTotalSupply(uint256 tokenId) private {
+    function _addTokensToAllTokensEnumeration(uint256 tokenId) private {
+        // this to determine the position (index) of the added token
+        _allTokensIndex[tokenId] = _allTokens.length;
         _allTokens.push(tokenId);
+    }
+
+    function _addTokensToOwnerEnumeration(address to, uint256 tokenId) private {
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+        _ownedTokens[to].push(tokenId);
+    }
+
+    function tokenByIndex(uint256 index) public view returns (uint256) {
+        require(index < totalSupply(), " global index out of bounds!");
+        return _allTokens[index];
+    }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        public
+        view
+        returns (uint256)
+    {
+        require(index < balanceOf(owner), " owner index out of bounds!");
+        return _ownedTokens[owner][index];
     }
 
     function totalSupply() public view returns (uint256) {
